caf181d0b3f2824eb628d5195b0d83525ea495a9
bgulbis <brian.gulbis@gmail.com>
2016-10-23 20:32:20 -0500

    work on reading in sample

--- src/1-make_sample.R
+++ src/1-make_sample.R
@@ -1,4 +1,16 @@
 # make_sample
 # 
-# make a sample of the text data
+# make a sample of each of the 3 text data files
 
+library(tidyverse)
+
+# make a file connection
+con <- file("data/raw/en_US.blogs.txt.gz", "rb")
+
+# check encoding, should be UTF-8
+# guess_encoding("data/raw/en_US.blogs.txt.gz")
+# guess_encoding("data/raw/en_US.news.txt.gz")
+# guess_encoding("data/raw/en_US.twitter.txt.gz")
+
+blog <- read_lines("data/raw/en_US.blogs.txt.gz", n_max = 10, skip = 5)
+close(con)
