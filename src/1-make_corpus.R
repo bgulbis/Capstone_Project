c6d9b7b5fa6d9b666e787a741aeed39800aadae4
bgulbis <brian.gulbis@gmail.com>
2016-11-08 18:21:20 -0600

    explore removing special characters

--- src/1-make_corpus.R
+++ src/1-make_corpus.R
@@ -13,11 +13,12 @@ profanity <- read_lines("data/external/profanity.txt")
 #' @param raw name of the text file to read in
 #' @param name name to append to each saved data file
 #' @param dir directory where the data files are saved; defaults to data/tidy
-make_corpus <- function(raw, name, dir = "data/tidy") {
+#' @param perc percent of lines to keep in corpus; defaults to 0.1 (10%)
+make_corpus <- function(raw, name, dir = "data/tidy", perc = 0.1) {
     x <- read_lines(raw) %>%
         str_to_lower()
     
-    keep <- map_lgl(x, ~ runif(1) >= 0.9)
+    keep <- map_lgl(x, ~ runif(1) >= 1 - perc)
     
     x <- x[keep]
     
@@ -38,6 +39,42 @@ make_corpus <- function(raw, name, dir = "data/tidy") {
 
 # read text --------------------------------------------
 
-make_corpus("data/raw/en_US.blogs.txt.gz", "blogs")
-make_corpus("data/raw/en_US.news.txt.gz", "news") 
-make_corpus("data/raw/en_US.twitter.txt.gz", "tweets") 
+make_corpus("data/raw/en_US.blogs.txt.gz", "blogs", perc = 0.03)
+make_corpus("data/raw/en_US.news.txt.gz", "news", perc = 0.03) 
+make_corpus("data/raw/en_US.twitter.txt.gz", "tweets", perc = 0.03) 
+
+blogs <- read_lines("data/raw/en_US.blogs.txt.gz") 
+blogs3 <- readLines("data/raw/en_US.blogs.txt.gz")
+
+y <- str_detect(blogs, "\\\xdf")
+
+x <- which(str_detect(blogs, "So little A, who's 7 now"))
+
+blogs2 <- str_replace_all(blogs3, "[^[:graph:]]", " ")
+
+blogs2[x]
+
+iconv(blogs2[x], "UTF-8", "UTF-8", sub = "")
+
+blogs[x]
+
+# corp <- Corpus(VectorSource(blogs))
+corp <- Corpus(DirSource("data/raw", pattern = "en_US.blogs.txt.gz", encoding = "ASCII", mode = "text"))
+
+corp[[1]]$content[x]
+
+t <- tm_map(corp, function(y) iconv(enc2utf8(y), sub = "byte"))
+
+low <- tm_map(corp, tolower)
+
+x <- blogs
+
+corp2 <- Corpus(VectorSource(x))
+low2 <- tm_map(corp, tolower)
+
+dtm <- DocumentTermMatrix(corp)
+
+x <- str_detect(blogs, "[^[:alnum:]]")
+y <- grepl("[^[:alnum:]]", blogs)
+
+    str_replace_all("[^[:alnum:]]", " ")
