a2c6fbbc6ca71e653891cd6392b3194e85196f49
bgulbis <brian.gulbis@gmail.com>
2016-11-10 21:02:50 -0600

    update conversion for non-english characters

--- src/1-make_corpus.R
+++ src/1-make_corpus.R
@@ -19,7 +19,8 @@ profanity <- read_lines("data/external/profanity.txt")
 #' @param perc percent of lines to keep in corpus; defaults to 0.1 (10%)
 make_corpus <- function(pattern, name, read_dir = "data/raw", save_dir = "data/tidy", perc = 0.1) {
     corp <- Corpus(DirSource("data/raw", pattern = pattern, encoding = "UTF-8", mode = "text")) %>%
-        tm_map(content_transformer(function(x) iconv(x, to = "UTF-8", sub = " "))) %>%
+        tm_map(content_transformer(function(x) iconv(x, from = "latin1", to = "ASCII", sub = " "))) %>%
+        # tm_map(content_transformer(function(x) iconv(x, to = "UTF-8", sub = " "))) %>%
         tm_map(content_transformer(tolower)) %>%
         tm_map(removeWords, profanity) %>%
         tm_map(removePunctuation) %>%
@@ -44,10 +45,22 @@ make_corpus("en_US.blogs.txt.gz", "blogs", perc = 0.03)
 make_corpus("en_US.news.txt.gz", "news", perc = 0.03) 
 make_corpus("en_US.twitter.txt.gz", "tweets", perc = 0.03) 
 
-# tmp <- read_lines("data/raw/en_US.blogs.txt.gz") 
+# tmp <- read_lines("data/raw/en_US.blogs.txt.gz")
 # x <- which(str_detect(tmp, "So little A, who's 7 now"))
 
 blogs <- read_rds("data/tidy/corpus_blogs.Rds")
 tdm <- TermDocumentMatrix(blogs)
 write_rds(tdm, "data/tidy/tdm_blogs.Rds")
+rm(blogs)
+
+news <- read_rds("data/tidy/corpus_news.Rds")
+tdm <- TermDocumentMatrix(news)
+write_rds(tdm, "data/tidy/tdm_news.Rds")
+rm(news)
+
+tweets <- read_rds("data/tidy/corpus_tweets.Rds")
+tdm <- TermDocumentMatrix(tweets)
+write_rds(tdm, "data/tidy/tdm_tweets.Rds")
+rm(tweets)
+
 
