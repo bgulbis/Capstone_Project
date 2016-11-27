06fcf9cd9cb2cad09954e32448c230c135cb825d
bgulbis <brian.gulbis@gmail.com>
2016-11-27 09:49:43 -0600

    add GT discounting

--- src/1a-quanteda.R
+++ src/1a-quanteda.R
@@ -1,58 +1,145 @@
 
 library(tidyverse)
 library(quanteda)
-# library(topicmodels)
+
+# helper functions -------------------------------------
+
+make_ngram <- function(x, n = 1L) {
+    col_name <- "phrase"
+
+    if (n == 1L) {
+        col_name <- "word1"
+        new_col <- "count1"
+    } else if (n == 2L) {
+        new_col <- "count2"
+    } else if (n == 3L) {
+        new_col <- "count3"
+    }
+    
+    dfm(x, 
+        toLower = TRUE, 
+        ignoredFeatures = c(profanity), 
+        removeNumbers = TRUE, 
+        removePunct = TRUE, 
+        removeSeparators = TRUE,
+        ngrams = n, 
+        concatenator = " "
+    ) %>%
+        docfreq() %>%
+        as_tibble() %>%
+        rownames_to_column(col_name) %>%
+        rename_(.dots = set_names(list(~value), new_col)) 
+}
+
+calc_mle <- function(x, y = NULL, n = 1L) {
+    sep_cols <- c("word1", "word2")
+    join_by <- "word1"
+
+    if (n == 1L) {
+        m <- list(~count1 / sum(count1))
+        nm <- "mle1"
+    } else if (n == 2L) {
+        m <- list(~count2 / count1)
+        nm <- "mle2"
+    } else if (n == 3L) {
+        sep_cols <- c(sep_cols, "word3")
+        join_by <- c(join_by, "word2")
+        m <- list(~count3 / count2)
+        nm <- "mle3"
+    }
+
+    if (!is.null(y)) {
+        x <- separate(x, "phrase", sep_cols, sep = " ") %>%
+            left_join(y, by = join_by)
+    }
+
+    mutate_(x, .dots = set_names(m, nm))
+}
+
+calc_discount <- function(r, m, n) {
+    if_else(r > 0 & r <= 5, ((r + 1) / r) * (n / m), 0) 
+}
+
+calc_prob_remain <- function(freq, disc, all_freq) {
+    1 - sum((disc * freq) / all_freq)
+}
+
+# read data --------------------------------------------
 
 profanity <- read_lines("data/external/profanity.txt") 
+
+size = 1000
 
 set.seed(77123)
 blogs <- read_lines("data/raw/en_US.blogs.txt.gz") %>%
     corpus() %>%
-    sample(size = 1000)
+    sample(size = size) %>%
+    tokenize("sentence", simplify = TRUE)
+
+set.seed(77123)
+news <- read_lines("data/raw/en_US.news.txt.gz") %>%
+    corpus() %>%
+    sample(size = size) %>%
+    tokenize("sentence", simplify = TRUE)
+
+set.seed(77123)
+tweets <- read_lines("data/raw/en_US.twitter.txt.gz") %>%
+    corpus() %>%
+    sample(size = size) %>%
+    tokenize("sentence", simplify = TRUE)
+
+words <- c(blogs, news, tweets)
+
+# make ngrams ------------------------------------------
+
+words_1gram <- make_ngram(words, 1L) %>%
+    calc_mle()
+
+words_2gram <- make_ngram(words, 2L) %>%
+    calc_mle(words_1gram, 2L)
+
+words_3gram <- make_ngram(words, 3L) %>%
+    calc_mle(words_2gram, 3L)
+
+# Good-Turing discount ---------------------------------
+gt_1gram <- table(words_1gram$count1) %>%
+    as_tibble() %>%
+    rename(uni = n) 
+
+gt_2gram <- table(words_2gram$count2) %>%
+    as_tibble() %>%
+    rename(bi = n) 
+
+gt_3gram <- table(words_3gram$count3) %>%
+    as_tibble() %>%
+    rename(tri = n) 
+
+gt_freq <- full_join(gt_1gram, gt_2gram, by = "Var1") %>%
+    full_join(gt_3gram, by = "Var1") %>%
+    rename(count = Var1) %>%
+    dmap_at("count", as.integer) %>%
+    arrange(count) %>%
+    mutate(uni_next = if_else(lead(count) == count + 1, lead(uni), 0L),
+           bi_next = if_else(lead(count) == count + 1, lead(bi), 0L),
+           tri_next = if_else(lead(count) == count + 1, lead(tri), 0L)) %>%
+    dmap(~ coalesce(.x, 0L)) 
+
+words_1gram_gt <- words_1gram %>%
+    left_join(gt_freq, by = c("count1" = "count")) %>%
+    mutate(discount = calc_discount(count1, uni, uni_next)) 
+
+words_2gram_gt <- words_2gram %>%
+    left_join(gt_freq, by = c("count2" = "count")) %>%
+    mutate(discount = calc_discount(count2, bi, bi_next)) %>%
+    group_by(word1) %>%
+    mutate(remain = calc_prob_remain(count2, discount, sum(count2)))
+
+words_3gram_gt <- words_3gram %>%
+    left_join(gt_freq, by = c("count3" = "count")) %>%
+    mutate(discount = calc_discount(count3, tri, tri_next)) %>%
+    group_by(word1, word2) %>%
+    mutate(remain = calc_prob_remain(count3, discount, sum(count3)))
     
-blogs_tokens <- tokenize(blogs, "sentence", simplify = TRUE)
-
-blogs_1gram <- dfm(blogs_tokens, toLower = TRUE, ignoredFeatures = c(profanity), 
-                   removeNumbers = TRUE, removePunct = TRUE, removeSeparators = TRUE)
-blogs_2gram <- dfm(blogs_tokens, toLower = TRUE, ignoredFeatures = c(profanity), 
-                   removeNumbers = TRUE, removePunct = TRUE, removeSeparators = TRUE, 
-                   ngrams = 2L, concatenator = " ")
-blogs_3gram <- dfm(blogs_tokens, toLower = TRUE, ignoredFeatures = c(profanity), 
-                   removeNumbers = TRUE, removePunct = TRUE, removeSeparators = TRUE, 
-                   ngrams = 3L, concatenator = " ")
-
-blogs_1gram_freq <- docfreq(blogs_1gram, scheme = "count") %>%
-    as_tibble() %>%
-    rownames_to_column("unigram")
-
-blogs_uni_freq <- tibble(uni = table(blogs_1gram_freq$value))
-
-blogs_2gram_freq <- docfreq(blogs_2gram, scheme = "count") %>%
-    as_tibble() %>%
-    rownames_to_column("bigram") %>%
-    separate(bigram, c("first", "second"), sep = " ", remove = FALSE)
-
-blogs_bi_freq <- tibble(bi = table(blogs_2gram_freq$value))
-
-blogs_3gram_freq <- docfreq(blogs_3gram, scheme = "count") %>%
-    as_tibble() %>%
-    rownames_to_column("trigram") %>%
-    separate(trigram, c("first", "second", "third"), sep = " ", remove = FALSE)
-
-blogs_tri_freq <- tibble(tri = table(blogs_3gram_freq$value))
-
-x <- blogs_tokens %>%
-    removeFeatures(profanity) %>%
-    collocations(method = "all", size = 2:3)
-
-y <- blogs_2gram_freq %>%
-    rename(count_bi = value) %>%
-    left_join(blogs_1gram_freq, by = c("first" = "unigram")) %>%
-    mutate(prob_bi = count_bi / value)
-
-z <- blogs_3gram_freq %>%
-    rename(count_tri = value,
-           tri1 = first,
-           tri2 = second) %>%
-    left_join(y, by = c("tri1" = "first", "tri2" = "second")) %>%
-    mutate(prob_tri = count_tri / count_bi)
+write_rds(words_1gram_gt, "data/final/initial_prediction_1gram_gt.Rds")
+write_rds(words_2gram_gt, "data/final/initial_prediction_2gram_gt.Rds")
+write_rds(words_3gram_gt, "data/final/initial_prediction_3gram_gt.Rds")
