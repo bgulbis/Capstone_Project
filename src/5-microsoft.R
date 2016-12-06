3d7d2ffd1bf7c999ae4ae569158ca3c17daf962f
bgulbis <brian.gulbis@gmail.com>
2016-12-05 23:25:41 -0600

    update api post

--- src/5-microsoft.R
+++ src/5-microsoft.R
@@ -4,6 +4,7 @@ library(httr)
 
 # Ocp-Apim-Subscription-Key
 api <- "39b702f50fd04d1f9730243bbbd6e0e8"
+names(api) <- "Ocp-Apim-Subscription-Key"
 
 url <- "https://api.projectoxford.ai/text/weblm/v1.0/generateNextWords"
 
@@ -12,5 +13,5 @@ url <- "https://api.projectoxford.ai/text/weblm/v1.0/generateNextWords"
 # order: 1-5, optional
 # maxNumOfCandidatesReturned, default = 5; optional
 
-POST(url = url, config = list(add_headers(Ocp-Apim-Subscription-Key = api)),
-     body = list(model = "body", words = "a case of"), encode = "json")
+r <- POST(url, add_headers(api))
+
