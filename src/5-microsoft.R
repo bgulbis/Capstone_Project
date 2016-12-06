8703968d6193786e733a074689f7528355eb6fec
Brian Gulbis <brian.gulbis@gmail.com>
2016-12-06 05:49:45 +0000

    create predict function

--- src/5-microsoft.R
+++ src/5-microsoft.R
@@ -1,6 +1,8 @@
 # microsoft api
 
 library(httr)
+library(jsonlite)
+library(stringr)
 
 # Ocp-Apim-Subscription-Key
 api <- "39b702f50fd04d1f9730243bbbd6e0e8"
@@ -13,5 +15,23 @@ url <- "https://api.projectoxford.ai/text/weblm/v1.0/generateNextWords"
 # order: 1-5, optional
 # maxNumOfCandidatesReturned, default = 5; optional
 
-r <- POST(url, add_headers(api), query = list(model = "body", words = "a case of"))
+predict_web <- function(x) {
+    words <- str_to_lower(x) %>%
+        word(-4, -1) 
+    
+    r <- POST(url, add_headers(api), 
+              query = list(model = "body", words = "and a case of"))
+    
+    fromJSON(content(r, "text"))$candidates
+}
 
+x1 <- predict_web("When you breathe, I want to be the air for you. I'll be there for you, I'd live and I'd")
+x2 <- predict_web("Guy at my table's wife got up to go to the bathroom and I asked about dessert and he started telling me about his")
+x3 <- predict_web("I'd give anything to see arctic monkeys this")
+x4 <- predict_web("Talking to your mom has the same effect as a hug and helps reduce your")
+x5 <- predict_web("When you were in Holland you were like 1 inch away from me but you hadn't time to take a")
+x6 <- predict_web("I'd just like all of these questions answered, a presentation of evidence, and a jury to settle the")
+x7 <- predict_web("I can't deal with unsymetrical things. I can't even hold an uneven number of bags of groceries in each")
+x8 <- predict_web("Every inch of you is perfect from the bottom to the")
+x9 <- predict_web("I’m thankful my childhood was filled with imagination and bruises from playing")
+x10 <- predict_web("I like how the same people are in almost all of Adam Sandler's")
