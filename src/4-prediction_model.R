3e55432eda9eadef62896e7f02d7f544887954e1
bgulbis <brian.gulbis@gmail.com>
2016-12-11 17:52:00 -0600

    remove quiz questions

--- src/4-prediction_model.R
+++ src/4-prediction_model.R
@@ -62,16 +62,3 @@ predict_text <- function(x, corp = "blogs", return_n = 5, keep_stop = FALSE) {
     
     pred[1:return_n]
 }
-
-library(microbenchmark)
-microbenchmark(x1 <- predict_text("The guy in front of me just bought a pound of bacon, a bouquet, and a case of", "blogs"), times = 5L)
-
-x2 <- predict_text("You're the reason why I smile everyday. Can you follow me please? It would mean the", "blogs", 10)
-x3 <- predict_text("Hey sunshine, can you follow me and make me the")
-x4 <- predict_text("Very early observations on the Bills game: Offense still struggling but the")
-x5 <- predict_text("Go on a romantic date at the")
-x6 <- predict_text("Well I'm pretty sure my granny has some old bagpipes in her garage I'll dust them off and be on my")
-x7 <- predict_text("Ohhhhh #PointBreak is on tomorrow. Love that film and haven't seen it in quite some")
-x8 <- predict_text("After the ice bucket challenge Louis will push his long wet hair out of his eyes with his little")
-x9 <- predict_text("Be grateful for the good times and keep the faith during the")
-x10 <- predict_text("If this isn't the cutest thing you've ever seen, then you must be")
