2549d75b10e3eaba93136f33fdde023dbbfe40e2
bgulbis <brian.gulbis@gmail.com>
2016-12-11 16:05:21 -0600

    update functions to use data.tables

--- src/3-make_probability_tables.R
+++ src/3-make_probability_tables.R
@@ -2,60 +2,41 @@
 
 library(tidyverse)
 library(stringr)
-# library(data.table)
-# 
-# calc_mle_dt <- function(freq) {
-#     x <- as.data.table(freq, TRUE)
-#     x[, n := str_count(rn, "_") + 1]
-#     x[, c("word1", "word2", "word3") := tstrsplit(rn, "_", fixed = TRUE)]
-#     x[n == 1, mle := freq / sum(freq)]
-#     x[n == 2, cnt := sum(freq), by = word1]
-#     x[n == 3, cnt := sum(freq), by = c("word1", "word2")]
-#     x[n > 1, mle := freq / cnt]
-# }
-# 
-# library(microbenchmark)
-# tokens_blogs <- read_rds("data/final/tokens_blogs2.Rds")
-# microbenchmark(x <- calc_mle_dt(tokens_blogs), times = 5)
-# 
-# tokens <- read_rds("data/final/tokens_blogs.Rds")
-# microbenchmark(y1 <- calc_mle(tokens), times = 5)
-# microbenchmark(y2 <- calc_mle(tokens, y1, 2L), times = 5)
-# microbenchmark(y3 <- calc_mle(tokens, y2, 3L), times = 5)
+library(data.table)
 
-calc_mle <- function(x, y = NULL, ng = 1L) {
-    sep_cols <- map_chr(1:ng, ~paste0("word", .x))
-    join_by <- map_chr(1:(ng - 1), ~paste0("word", .x))
+calc_mle_dt <- function(uni, bi, tri) {
+    x <- as.data.table(uni, TRUE)
+    y <- as.data.table(bi, TRUE)
+    z <- as.data.table(tri, TRUE)
     
-    if (ng == 1L) {
-        m <- list(~value / sum(value))
-    } else {
-        m <- list(lazyeval::interp(~ value / cnt, cnt = as.name(paste0("count", ng - 1))))
-    }
+    x[, mle1 := uni / sum(uni)]
+    setnames(x, c("rn", "uni"), c("word1", "count1"))
     
-    x <- filter_(x, .dots = list(~n == ng)) %>%
-        separate_("ngram", sep_cols, sep = "_")
+    y[, c("word1", "word2") := tstrsplit(rn, "_", fixed = TRUE)]
+    y[, freq2 := sum(bi), by = word1]
+    y[, mle2 := bi / freq2]
+    y[, rn := NULL]
+    setnames(y, "bi", "count2")
     
-    if (!is.null(y)) {
-        x <- left_join(x, y, by = join_by)
-    }
-    
-    x %>%
-        mutate_(.dots = set_names(m, "mle")) %>%
-        select_(.dots = c(as.list(sep_cols), list(~value, ~mle))) %>%
-        rename_(.dots = set_names(list(~value, ~mle), list(paste0("count", ng), paste0("mle", ng)))) 
+    z[, c("word1", "word2", "word3") := tstrsplit(rn, "_", fixed = TRUE)]
+    z[, freq3 := sum(tri), by = c("word1", "word2")]
+    z[, mle3 := tri / freq3]
+    z[, rn := NULL]
+    setnames(z, "tri", "count3")
+
+    list(x, y, z)
 }
 
-calc_good_turing <- function(x, y, z) {
-    gt_1gram <- table(x$count1) %>%
+calc_gt_dt <- function(x) {
+    gt_1gram <- table(x[[1]]$count1) %>%
         as_tibble() %>%
         rename(uni = n) 
     
-    gt_2gram <- table(y$count2) %>%
+    gt_2gram <- table(x[[2]]$count2) %>%
         as_tibble() %>%
         rename(bi = n) 
     
-    gt_3gram <- table(z$count3) %>%
+    gt_3gram <- table(x[[3]]$count3) %>%
         as_tibble() %>%
         rename(tri = n) 
     
@@ -78,40 +59,42 @@ calc_prob_remain <- function(disc, mle) {
     1 - sum(disc * mle)
 }
 
-make_prob_tables <- function(x, corp) {
-    words_1gram <- calc_mle(x)
-    words_2gram <- calc_mle(x, words_1gram, 2L)
-    words_3gram <- calc_mle(x, words_2gram, 3L)
-    gt_freq <- calc_good_turing(words_1gram, words_2gram, words_3gram)
+make_prob_dt <- function(x, y, z, corp) {
+    mle <- calc_mle_dt(x, y, z)
+    gt_freq <- calc_gt_dt(mle)
     
-    pred_3gram <- words_3gram %>%
+    pred_3gram <- mle[[3]] %>%
         left_join(gt_freq, by = c("count3" = "count")) %>%
         mutate(discount = calc_discount(count3, tri, tri_next)) %>%
         group_by(word1, word2) %>%
         mutate(remain = calc_prob_remain(discount, mle3))
     
-    pred_2gram <- words_2gram %>%
+    pred_2gram <- mle[[2]] %>%
         left_join(gt_freq, by = c("count2" = "count")) %>%
         mutate(discount = calc_discount(count2, bi, bi_next)) %>%
         group_by(word1) %>%
         mutate(remain = calc_prob_remain(discount, mle2))
     
-    pred_1gram <- words_1gram %>%
+    pred_1gram <- mle[[1]] %>%
         left_join(gt_freq, by = c("count1" = "count")) %>%
         mutate(discount = calc_discount(count1, uni, uni_next)) %>%
         group_by(word1) %>%
         mutate(remain = calc_prob_remain(discount, mle1))
+
+    write_rds(pred_1gram, paste0("data/final/pred_1gram_", corp, ".Rds"), compress = "gz")
+    write_rds(pred_2gram, paste0("data/final/pred_2gram_", corp, ".Rds"), compress = "gz")
+    write_rds(pred_3gram, paste0("data/final/pred_3gram_", corp, ".Rds"), compress = "gz")
     
-    write_rds(pred_1gram, paste0("data/final/pred_1gram_", corp, ".Rds"))
-    write_rds(pred_2gram, paste0("data/final/pred_2gram_", corp, ".Rds"))
-    write_rds(pred_3gram, paste0("data/final/pred_3gram_", corp, ".Rds"))
 }
 
-read_rds("data/final/tokens_blogs.Rds") %>%
-    make_prob_tables("blogs")
+# read in all of the tokens files
+x <- list.files("data/tidy", "tokens_", full.names = TRUE)
+nm <- list.files("data/tidy", "tokens_")
+nm <- str_replace_all(nm, ".Rds", "")
+files <- map(x, read_rds)
+names(files) <- nm
+list2env(files, .GlobalEnv)
 
-read_rds("data/final/tokens_news.Rds") %>%
-    make_prob_tables("news")
-
-read_rds("data/final/tokens_tweets.Rds") %>%
-    make_prob_tables("tweets")
+make_prob_dt(tokens_blogs1, tokens_blogs2, tokens_blogs3, "blogs")
+make_prob_dt(tokens_news1, tokens_news2, tokens_news3, "news")
+make_prob_dt(tokens_tweets1, tokens_tweets2, tokens_tweets3, "tweets")
